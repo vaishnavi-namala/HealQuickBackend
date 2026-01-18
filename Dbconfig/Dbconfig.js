09ebcc732a7ab7bd460c8bf51c3709425e28cca8
vaishnavi-namala <vaishnavinamala7@gmail.com>
2026-01-18 23:40:07 +0530

    Update Dbconfig.js

--- Dbconfig/Dbconfig.js
+++ Dbconfig/Dbconfig.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const conurl="mongodb+srv://vaishnavinamala7_db_user:<db_password>@healquick.ztq4r01.mongodb.net/?appName=HEALQUICK"
+const conurl="mongodb+srv://vaishnavinamala7_db_user:qpIoXkrKVhzi2JMc@healquick.ztq4r01.mongodb.net/?appName=HEALQUICK"
 mongoose.connect(conurl)
 .then(con=>console.log("connect"))
 .catch(err=>console.log(err))
